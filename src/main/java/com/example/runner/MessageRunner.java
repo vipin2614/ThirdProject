bac75ac578a35ff644ff2dd8c6e59d1d11059add
vipin <vipin@pyro-HP-240-G6-Notebook-PC>
2020-07-23 15:14:33 +0530

    WH-01-First Time File Update

---
+++ src/main/java/com/example/runner/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.example.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("......Inside Message Runner.....");
+
+	}
+
+}
